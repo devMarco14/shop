02ed75a5982d7c758adb5c03c3527380c663fc27
Marco <jonghyeok.lim14@gmail.com>
2022-09-01 16:36:39 +0900

    react router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import { useState } from "react";
 import data from "./data.js";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   let [shoes] = useState(data);
@@ -17,12 +18,55 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
-      <div className="main-bg"></div>
-      <div className="container">
-        <div className="row">
-          {shoes.map((a, i) => (
-            <Card shoes={shoes[i]} i={i} />
-          ))}
+
+      <Link to="/">홈</Link>
+      <Link to="/detail">상세페이지</Link>
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="main-bg"></div>
+              <div className="container">
+                <div className="row">
+                  {shoes.map((id, index) => (
+                    <Card shoes={shoes[index]} i={index} />
+                  ))}
+                </div>
+              </div>
+            </>
+          }
+        />
+        <Route
+          path="/detail"
+          element={
+            <>
+              <Detail />
+            </>
+          }
+        />
+      </Routes>
+    </div>
+  );
+}
+
+function Detail(props) {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6">
+          <img
+            src="https://codingapple1.github.io/shop/shoes1.jpg"
+            width="100%"
+            alt="shoes"
+          />
+        </div>
+        <div className="col-md-6">
+          <h4 className="pt-5">상품명</h4>
+          <p>상품설명</p>
+          <p>120000원</p>
+          <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
     </div>
@@ -36,10 +80,11 @@ function Card(props) {
         src={
           "https://codingapple1.github.io/shop/shoes" + (props.i + 1) + ".jpg"
         }
+        alt="shoes"
         width="80%"
       />
       <h4>{props.shoes.title}</h4>
-      <p>{props.shoes.price}</p>
+      <p>{props.shoes.price}원</p>
     </div>
   );
 }
